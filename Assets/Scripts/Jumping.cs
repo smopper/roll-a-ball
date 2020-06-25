05a31a0e53714dde5a628c68116d4ef82a56cd24
smopper <57532046+smopper@users.noreply.github.com>
2020-06-25 23:23:39 +0100

    Add files via upload

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Jumping : MonoBehaviour
+{
+    public void DeleteSaves()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
